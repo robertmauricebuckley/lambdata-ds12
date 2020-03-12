482ec8f500bf18bfe6808e9b0d646e64da5f8a5e
Robert Buckley <robertworkbuckley@gmail.com>
2020-03-12 15:36:15 -0700

    unittest

---
+++ test/my_mod_test.py
@@ -0,0 +1,12 @@
+import unittest
+
+from my_lambdata.my_mod import enlarge
+
+class TestStringMethods(unittest.TestCase):
+
+    def test_enlarge(self):
+        self.assertEqual(enlarge(5), 500)
+
+
+if __name__ == '__main__':
+    unittest.main()
